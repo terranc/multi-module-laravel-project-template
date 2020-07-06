dbb995ff7b8542df90baa483f5408c52d341c5ec
TerranChao <wuchaoboy@gmail.com>
2020-07-06 09:49:49 +0800

    model append example

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
 
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read mixed $base64_email
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class User extends Base {
+    protected $appends = [
+        'email' => 'base64_email'   // 当查询数据中存在 email 时才 append
+    ];
+
+    public function getBase64EmailAttribute() {
+        return base64_encode($this->email);
+    }
 }
 
--- modules/Common/Http/Controllers/CommonController.php
+++ modules/Common/Http/Controllers/CommonController.php
@@ -3,10 +3,15 @@
 namespace Modules\Common\Http\Controllers;
 
 use App\Exceptions\Api\ApiNotFoundException;
+use App\Exceptions\DontReportApiException;
+use App\Models\User;
+use Illuminate\Http\Request;
 
 class CommonController extends Controller {
-    public function ping() {
-        throw new ApiNotFoundException('无可用记录');
-        return $this->success('SUCCESS');
+    public function ping(Request $request) {
+        if ($request->error) {
+            throw new ApiNotFoundException('无数据');
+        }
+        return $this->success('SUCCESS', User::first());
     }
 }
